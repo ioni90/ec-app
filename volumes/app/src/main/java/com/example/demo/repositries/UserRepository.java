27cb91f0e4e51576baa1c10c0e39b2d6a91f3d52
ioni90 <kkoioni27@gmail.com>
2021-02-26 23:39:52 +0900

    UserRepository記述

---
+++ volumes/app/src/main/java/com/example/demo/repositries/UserRepository.java
@@ -0,0 +1,15 @@
+package com.example.demo.repositries;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String>{
+	Optional<User> findById(String id);
+	List<User> findAll();
+}
